7975aef5a575b38ae56b115d7bfbf7816f94d545
srdjan-at-2c <srdjan.stankovic@2c-solution.net>
2025-12-22 15:06:25 +0100

    please work

--- deploy/cypress/cypress.config.cjs
+++ deploy/cypress/cypress.config.cjs
@@ -1,11 +1,8 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL,
-    setupNodeEvents(on, config) {
-      return config;
-    },
 
     specPattern: "src/e2e/**/*.cy.{js,ts}",
     supportFile: "src/support/e2e.ts",
