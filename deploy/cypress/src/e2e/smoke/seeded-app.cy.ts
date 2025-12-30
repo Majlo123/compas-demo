cbc0f1fd557291b7b05d4056b455c051e25ec977
Skipina <vladimir.skipina@gmail.com>
2025-12-30 12:13:43 +0100

    feat: fix tests

--- deploy/cypress/src/e2e/smoke/seeded-app.cy.ts
+++ deploy/cypress/src/e2e/smoke/seeded-app.cy.ts
@@ -6,21 +6,21 @@ describe("Seeded application", () => {
   });
 
   it("shows warning levels seeded in the database", () => {
-    cy.contains("Warning Levels");
+    cy.contains("PAR Level Management");
 
     cy.contains("Warning level 1");
-    cy.contains("warning level 1 description");
+    cy.contains("Level 1");
 
     cy.contains("Warning level 2");
-    cy.contains("warning level 2 description");
+    cy.contains("Level 2");
 
     cy.contains("Warning level 3");
-    cy.contains("warning level 3 description");
+    cy.contains("Level 3");
 
     cy.contains("Warning level 4");
-    cy.contains("warning level 4 description");
+    cy.contains("Level 4");
 
     cy.contains("Warning level 5");
-    cy.contains("warning level 5 description");
+    cy.contains("Level 5");
   });
 });
